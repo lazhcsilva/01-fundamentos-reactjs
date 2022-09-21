16e1b7b3fff372f6eb9b33c5a8e72f051cb0853c
Lázaro Silva <lazhcsilva@gmail.com>
2022-09-21 00:01:24 -0300

    Inserting Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post"
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
@@ -8,14 +10,19 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores libero atque quidem voluptatem iusto facere doloribus, tempora ad beatae accusamus aperiam? Ullam ea suscipit a vel sunt, ratione ducimus expedita."
-      />
-      <Post 
-        author="Polly Mascarenhas"
-        content="Conteudo indefinido"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores libero atque quidem voluptatem iusto facere doloribus, tempora ad beatae accusamus aperiam? Ullam ea suscipit a vel sunt, ratione ducimus expedita."
+          />
+          <Post
+            author="Polly Mascarenhas"
+            content="Conteudo indefinido"
+          />
+        </main>
+      </div>
     </div>
   )
 }
